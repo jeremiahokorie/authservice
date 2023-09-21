c3c0e71d717750b60beddf16afff55418dc9bfbf
Jeremiah Imo <jeremiah.imo@etranzact.com>
2023-09-21 16:01:07 +0100

    added database credentials to environment variable

--- src/main/java/com/authservice/config/WebSecurityConfig.java
+++ src/main/java/com/authservice/config/WebSecurityConfig.java
@@ -1,11 +1,14 @@
 package com.authservice.config;
 
+import com.authservice.repository.UserRepository;
+import org.apache.catalina.security.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -26,6 +29,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 // extends WebSecurityConfigurerAdapter
 
     UserDetailsService userDetailsService;
+
+    UserRepository userRepository;
+
 
 //
 //    @Bean
@@ -65,5 +71,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
        return authenticationConfiguration.getAuthenticationManager();
     }
+
+
 
 }
